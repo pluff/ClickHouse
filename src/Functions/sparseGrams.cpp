f5a4bc03339ab6eaa51b0c80a11329d598d734a3
Pervakov Grigorii <pervakov.grigory@gmail.com>
2025-03-24 16:51:29 +0100

    fix style and cleanup, fix docs

--- src/Functions/sparseGrams.cpp
+++ src/Functions/sparseGrams.cpp
@@ -14,7 +14,6 @@ namespace DB
 namespace ErrorCodes
 {
 extern const int BAD_ARGUMENTS;
-extern const int ILLEGAL_COLUMN;
 extern const int ILLEGAL_TYPE_OF_ARGUMENT;
 extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
 }
@@ -139,6 +138,7 @@ public:
         left = 0;
         right = 1;
 
+        ngram_hashes.clear();
         if constexpr (is_utf8)
             utf8_offsets.clear();
 
@@ -242,11 +242,31 @@ using FunctionSparseGramsUTF8 = FunctionTokens<SparseGramsImpl<true>>;
 
 REGISTER_FUNCTION(SparseGrams)
 {
-    factory.registerFunction<FunctionSparseGrams>();
-    factory.registerFunction<FunctionSparseGramsUTF8>();
+    const FunctionDocumentation description = {
+        .description=R"(Finds all substrings of a given string that have a length of at least `n`,
+where the hashes of the (n-1)-grams at the borders of the substring
+are strictly greater than those of any (n-1)-gram inside the substring.)",
+        .arguments={
+            {"s", "An input string"},
+            {"min_ngram_length", "The minimum length of extracted ngram. The default and minimal value is 3"},
+        },
+        .returned_value="An array of selected substrings",
+        .category{"String"}
+    };
+    const FunctionDocumentation hashes_description {
+        .description = R"(Finds hashes of all substrings of a given string that have a length of at least `n`,
+where the hashes of the (n-1)-grams at the borders of the substring
+are strictly greater than those of any (n-1)-gram inside the substring.)",
+        .arguments = description.arguments,
+        .returned_value="An array of selected substrings hashes",
+        .category = description.category
+    };
 
-    factory.registerFunction<SparseGramsHashes<false>>();
-    factory.registerFunction<SparseGramsHashes<true>>();
+    factory.registerFunction<FunctionSparseGrams>(description);
+    factory.registerFunction<FunctionSparseGramsUTF8>(description);
+
+    factory.registerFunction<SparseGramsHashes<false>>(hashes_description);
+    factory.registerFunction<SparseGramsHashes<true>>(hashes_description);
 }
 
 }
